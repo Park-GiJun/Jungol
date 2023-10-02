1ebeb899745510b1f9155abccc3feae17f7b19c7
Park-GiJun <144126780+Park-GiJun@users.noreply.github.com>
2023-10-02 16:52:12 +0900

    10/02

---
+++ src/q9008/Main.java
@@ -0,0 +1,20 @@
+package q9008;
+
+
+//화면에 다음과 같이 출력하는 프로그램을 작성하시오.
+//
+//"="은 14개를 출력합니다.
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.print(" subject score\r\n"
+				+ "==============\r\n"
+				+ "  korean    90\r\n"
+				+ " english   100\r\n"
+				+ "computer    80");
+
+	}
+
+}
