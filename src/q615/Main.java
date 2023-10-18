3e7899364efb163085898185526f175dcee5e46c
tpgj9 <tpgj9@GiJun>
2023-10-18 16:43:03 +0900

    615

---
+++ src/q615/Main.java
@@ -0,0 +1,22 @@
+package q615;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		String[] arr1 = sc.nextLine().split(" ");
+		String[] arr2 = sc.nextLine().split(" ");
+
+		System.out.println(arr1[0] + " " + arr1[1] + " " + arr1[2]);
+		System.out.println(arr2[0] + " " + arr2[1] + " " + arr2[2]);
+
+		System.out.println("avg" + " " + (Integer.parseInt(arr1[1]) + Integer.parseInt(arr2[1])) / 2 + " "
+				+ (Integer.parseInt(arr1[2]) + Integer.parseInt(arr2[2])) / 2);
+	}
+
+}
